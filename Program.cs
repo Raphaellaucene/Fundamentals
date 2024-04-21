5ad785096e433636ce034be464645a17125ecf39
wa77p <rp.raphaelrs@hotmail.com>
2024-04-21 14:29:47 -0300

    modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,21 @@ pessoa1.Age = 29;
 pessoa1.Greetings();
 
 //casting -> conversão
+//a diferença do tratamento do nulo, em convert retorna 0, parse da erro (não pode ser nulo)
 //usando classe convert e metodo toInt32 
 int a = Convert.ToInt32("5");
 
+//parse
 int b = int.Parse("5");
+
+//para converter para string só usar toString pois ja vem de herança da classe mae (classe object)
+string c = a.ToString();
+
+//cast implicito, de int para double, long faz diretamente
+
+string d = "15-";
+
+int e = 0;
+
+//tentar converter se não der, a saida sera e
+int.TryParse(d, out e);
